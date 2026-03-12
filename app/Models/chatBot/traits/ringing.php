0e3c09bc357049ac47a88227ad0e42cc5ac54b51
kwtpub <10.0.kwt@mail.ru>
2026-03-12 03:44:24 +0300

    Update ringing.php

--- app/Models/chatBot/traits/ringing.php
+++ app/Models/chatBot/traits/ringing.php
@@ -4,9 +4,9 @@ namespace App\Models\chatBot\traits;
 
 trait ringing
 {
-    public function ringing($edit = 1)
+    public function ringing()
     {
-        // $this->main->keyBoard->add("Как считается скидка?", "discount_help");
+        $this->main->keyBoard->add("Как считается скидка?", "discount_help");
         // $this->main->keyBoard->add("Назад", "start");
 
         // $text = implode("\n", [
@@ -16,15 +16,7 @@ trait ringing
         //     "",
         // ]);
         //
+
         return $this->sendOrEditMsg("Введите номер жертвы");
     }
-
-    // public function discount_help($edit = 1)
-    // {
-    //     $this->main->keyBoard->add("Назад", "help");
-
-    //     $text = implode("\n", ["<b>Как считается скидка</b>", "НЭТ"]);
-
-    //     return $this->sendOrEditMsg($edit, $text);
-    // }
 }
