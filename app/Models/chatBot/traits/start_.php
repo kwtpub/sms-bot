aaf44dd7dec5e066d896c507f574756ab5407c12
kwtpub <10.0.kwt@mail.ru>
2026-03-12 04:17:51 +0300

    Update start_.php

--- app/Models/chatBot/traits/start_.php
+++ app/Models/chatBot/traits/start_.php
@@ -6,13 +6,25 @@ trait start_
     public function start($edit = 1)
     {
         $this->main->keyBoard->add("Начать прозвон", "ringing");
-        $this->main->keyBoard->add("Реферальная ссылка", "referral_info");
         $this->main->keyBoard->add("Личный кабинет", "lk");
         $this->main->keyBoard->add("Помощь", "help");
         if ($this->main->user->is_admin) {
             $this->main->keyBoard->add("Админ-панель", "admin_lk");
         }
 
-        return $this->sendOrEditMsg($edit, $this->buildStartText());
+        $text = implode("\n", [
+            "<b>Главный экран</b>",
+            "",
+            "Ваш ID: <code>" . $this->main->user->tg_id . "</code>",
+            "Баланс: <b>" .
+            $this->formatMoney($this->main->user->balance) .
+            "</b>",
+            "Скидка: <b>+" . $this->getDiscountPercent() . "%</b>",
+            "",
+            "Приглашай друзей и получай 10% от их трат себе на счет.",
+            "<code>' . e($this->getReferralLink()) . '</code>",
+        ]);
+
+        return $this->sendOrEditMsg($edit, $text);
     }
 }
